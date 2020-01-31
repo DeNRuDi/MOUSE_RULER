0d062312288ef5f939fdefae8f691187aba7bfe3
Denis Rudnitskiy <51990319+DeNRuDi@users.noreply.github.com>
2020-02-01 00:44:53 +0200

    v1.0

---
+++ MOUSE_RULER.py
@@ -0,0 +1,30 @@
+from pynput import mouse
+from pynput import keyboard
+print("\nMOUSE_RULER v1.0\nCopyright © 2020 DeNRuDi (Denis Rudnitskiy)\n")
+i = 1
+def main():
+	def on_press(key):
+		if key == keyboard.Key.esc:
+			return False	
+
+	def on_scroll(x, y, dx, dy):
+		global i
+		print(i, end='\r')
+		i+=1
+		
+	with mouse.Listener(on_scroll=on_scroll), keyboard.Listener(on_press=on_press) as listener:
+		listener.join()
+	print()
+	x = float(i - 1) / 3.4
+	print("Твой результат: " + str(round((x),2)) + " Cм\n")
+	
+while True:
+	answer = input("[1]Измерить предмет колёсиком мыши\n[2]Выход\n\nВведите ответ>")
+	if answer == str(1):
+		i = 1
+		print("После измерения, нажмите - 'ESC'")
+		main()
+	elif answer == str(2):
+		break
+	else:
+		print("\nВведен неверный ответ!\n")
